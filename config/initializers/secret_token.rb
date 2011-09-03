cdea9663cdd00606dde8aa97b52f01671a1760fb
mehbeh <zach.millman@gmail.com>
2011-09-03 00:07:22 -0700

    Initial commit of app scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoosballLeague::Application.config.secret_token = '48d5a81acb7fe37080808f2636ae73adb51deee3dc795dde5ec82c0f3aaf14a7d6679b176725729bd9c07dcbd0e6a81c514a3db64981d7c02334ebc15e50b8b7'
