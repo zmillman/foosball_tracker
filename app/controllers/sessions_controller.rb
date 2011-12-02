96cfda842eccb3c59bf8cfffdbb4fa5b40a50fb8
Zach Millman <zach.millman@gmail.com>
2011-12-01 23:13:38 -0800

    Redirect to league on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       self.current_user = user
-      redirect_to dashboard_path
+      if current_user.leagues.exists?
+        redirect_to [current_user.leagues.first]
+      else
+        redirect_to dashboard_path
+      end
     else
       flash.now.alert = "Invalid email or password"
       render "new"
